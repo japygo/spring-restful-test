598bac6059496c0941d4a1dd9fd2abb663e65b42
japygo <hyunnjinn@gmail.com>
2022-12-23 23:30:36 +0900

    init project

---
+++ src/main/java/com/example/springboot/SpringRestfulTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestfulTestApplication.class, args);
+    }
+
+}
